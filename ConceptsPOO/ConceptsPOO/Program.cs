610088572091e9889f340f9e212e5fe8e6bd7518
Gerald Then <gthen@mdsoft.com.do>
2022-04-28 07:21:34 -0400

    Employee, SalaryEmployee & CommissionEmployee

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -4,13 +4,29 @@ using ConceptsPOO;
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
 
-try
+Employee employee1 = new SalaryEmployee()
 {
-    Console.WriteLine(new Date(2024, 2, 29));
-    Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine(new Date(1985, 11, 30));
-}
-catch (Exception error)
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 23400.45M
+};
+
+Employee employee2 = new CommissionEmployee()
 {
-    Console.WriteLine(error.Message);
-}
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Perez",
+    BirthDate = new Date(1995, 7, 23),
+    HiringDate = new Date(2022, 3, 5),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+
+};
+
+Console.WriteLine(employee1);
+Console.WriteLine(employee2);
